588d00893c569730a37e33c15528f2a7d355f65a
Jory Geerts <github@jorygeerts.nl>
2021-07-23 18:07:26 +0200

    Read both pins, set webserver

--- server.go
+++ server.go
@@ -11,6 +11,9 @@ import (
 	"time"
 )
 
+var readyOne = false
+var readyTwo = false
+
 func main() {
 	c, err := gpiod.NewChip("gpiochip0")
 	if err != nil {
@@ -18,10 +21,10 @@ func main() {
 	}
 	defer c.Close()
 
-	l, err := c.RequestLine(rpi.GPIO21,
+	l1, err := c.RequestLine(rpi.GPIO21,
 		gpiod.WithPullUp,
 		gpiod.WithBothEdges,
-		gpiod.WithEventHandler(eventHandler))
+		gpiod.WithEventHandler(eventHandler(&readyOne)))
 	if err != nil {
 		fmt.Printf("RequestLine returned error: %s\n", err)
 		if err == syscall.Errno(22) {
@@ -29,14 +32,25 @@ func main() {
 		}
 		os.Exit(1)
 	}
-	defer l.Close()
+	defer l1.Close()
+
+	l2, err := c.RequestLine(rpi.GPIO20,
+		gpiod.WithPullUp,
+		gpiod.WithBothEdges,
+		gpiod.WithEventHandler(eventHandler(&readyTwo)))
+	if err != nil {
+		fmt.Printf("RequestLine returned error: %s\n", err)
+		if err == syscall.Errno(22) {
+			fmt.Println("Note that the WithPullUp option requires kernel V5.5 or later - check your kernel version.")
+		}
+		os.Exit(1)
+	}
+	defer l2.Close()
 
 	fs := http.FileServer(http.Dir("./assets"))
 
-	isReady := false
-
 	http.HandleFunc("/is-ready", func(writer http.ResponseWriter, request *http.Request) {
-		if isReady {
+		if readyOne && readyTwo {
 			writer.Write([]byte("yes"))
 		} else {
 			writer.Write([]byte("no"))
@@ -44,24 +58,24 @@ func main() {
 	})
 	http.Handle("/", fs)
 
-	go func() {
-		time.AfterFunc(time.Minute, func() {
-			isReady = true
-		})
-	}()
-
 	log.Fatal(http.ListenAndServe(":7777", nil))
 }
 
-func eventHandler(evt gpiod.LineEvent) {
-	t := time.Now()
-	edge := "rising"
-	if evt.Type == gpiod.LineEventFallingEdge {
-		edge = "falling"
+func eventHandler(b *bool) func(evt gpiod.LineEvent) {
+
+	return func(evt gpiod.LineEvent) {
+		t := time.Now()
+		edge := "rising"
+		if evt.Type == gpiod.LineEventFallingEdge {
+			edge = "falling"
+		}
+		fmt.Printf("event:%3d %-7s %s (%s)\n",
+			evt.Offset,
+			edge,
+			t.Format(time.RFC3339Nano),
+			evt.Timestamp)
+
+		*b = true
+
 	}
-	fmt.Printf("event:%3d %-7s %s (%s)\n",
-		evt.Offset,
-		edge,
-		t.Format(time.RFC3339Nano),
-		evt.Timestamp)
 }
