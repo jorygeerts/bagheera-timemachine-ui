f538e182197ba8a9fd10e16c41dc4f3fda8ec3bc
Jory Geerts <github@jorygeerts.nl>
2021-07-23 17:53:12 +0200

    GPIO

--- server.go
+++ server.go
@@ -1,12 +1,36 @@
 package main
 
 import (
+	"fmt"
+	"github.com/warthog618/gpiod"
+	"github.com/warthog618/gpiod/device/rpi"
 	"log"
 	"net/http"
+	"os"
+	"syscall"
 	"time"
 )
 
 func main() {
+	c, err := gpiod.NewChip("gpiochip0")
+	if err != nil {
+		panic(err)
+	}
+	defer c.Close()
+
+	l, err := c.RequestLine(rpi.GPIO21,
+		gpiod.WithPullUp,
+		gpiod.WithBothEdges,
+		gpiod.WithEventHandler(eventHandler))
+	if err != nil {
+		fmt.Printf("RequestLine returned error: %s\n", err)
+		if err == syscall.Errno(22) {
+			fmt.Println("Note that the WithPullUp option requires kernel V5.5 or later - check your kernel version.")
+		}
+		os.Exit(1)
+	}
+	defer l.Close()
+
 	fs := http.FileServer(http.Dir("./assets"))
 
 	isReady := false
@@ -27,4 +51,17 @@ func main() {
 	}()
 
 	log.Fatal(http.ListenAndServe(":7777", nil))
+}
+
+func eventHandler(evt gpiod.LineEvent) {
+	t := time.Now()
+	edge := "rising"
+	if evt.Type == gpiod.LineEventFallingEdge {
+		edge = "falling"
+	}
+	fmt.Printf("event:%3d %-7s %s (%s)\n",
+		evt.Offset,
+		edge,
+		t.Format(time.RFC3339Nano),
+		evt.Timestamp)
 }
