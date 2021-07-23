8e797202de7a7514832968a9a854c0b05b393e68
Jory Geerts <github@jorygeerts.nl>
2021-07-23 17:23:28 +0200

    Support "booting"

--- server.go
+++ server.go
@@ -3,12 +3,28 @@ package main
 import (
 	"log"
 	"net/http"
+	"time"
 )
 
 func main() {
 	fs := http.FileServer(http.Dir("./assets"))
 
+	isReady := false
+
+	http.HandleFunc("/is-ready", func(writer http.ResponseWriter, request *http.Request) {
+		if isReady {
+			writer.Write([]byte("yes"))
+		} else {
+			writer.Write([]byte("no"))
+		}
+	})
 	http.Handle("/", fs)
+
+	go func() {
+		time.AfterFunc(time.Minute, func() {
+			isReady = true
+		})
+	}()
 
 	log.Fatal(http.ListenAndServe(":7777", nil))
 }
